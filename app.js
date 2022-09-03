06f01b292f69e667f4cca4530d6ca32ce236a438
mojammel127 <mojammel127@gmail.com>
2022-09-03 15:16:11 +0600

    N/A part done and sume updatet

--- app.js
+++ app.js
@@ -45,12 +45,12 @@ const category = await fetch(`https://openapi.programming-hero.com/api/news/cate
                 
                 <div class="flex flex-row items-center">
                 <div><p><img src="${author.img}" class="h-24 w-24 p-4 rounded-full"</p></div>
-                <div class="basis-1/4 "><p>${author.name}</p></div>
+                <div class="basis-1/4 "><p>${author.name ? author.name : 'N/A'}</p></div>
                 <div class="ps-6"><svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path fill="none" stroke="#000" stroke-width="2" d="M12,21 C7,21 1,16 1,12 C1,8 7,3 12,3 C17,3 23,8 23,12 C23,16 17,21 12,21 Z M12,7 C9.23875,7 7,9.23875 7,12 C7,14.76125 9.23875,17 12,17 C14.76125,17 17,14.76125 17,12 C17,9.23875 14.76125,7 12,7 L12,7 Z"/>
                 </svg></div>
 
-                <div class="basis-1/2 p-4">${total_view} M</div>
+                <div class="basis-1/2 p-4">Total view ${total_view ? total_view : 'N/A'}</div>
                 <label onclick="newsDetail('${_id}')" for="my-modal-5" class="btn modal-button">more...</label>
                 
                 </div>
@@ -73,12 +73,12 @@ const modalData = (details)=>{
     const modalBody = document.getElementById('modal-body');;
     modalBody.innerHTML =`
     <div>
-            <h1 class="font-bold text-lg text-purple-700 text-3xl">Title: ${details.data[0].title}</h1>     
-            <h3 class="font-bold text-lg">Writer Name: ${details.data[0].author.name}</h3>
+            <h1 class="font-bold text-lg text-purple-700 text-3xl">${details.data[0].title}</h1>     
+            <h3 class="font-bold text-lg">Writer Name: ${details.data[0].author.name ? details.data[0].author.name : 'N/A'}</h3>
             <h3 class="font-bold text-lg">published_date: ${details.data[0].author.published_date}</h3>
             <div><p><img src="${details.data[0].thumbnail_url}" class=""</p></div>
             <h1 class="font-bold text-lg">News: ${details.data[0].details}</h1>
-            <h1 class="font-bold text-lg">Total view: ${details.data[0].total_view}</h1>
+            <h1 class="font-bold text-lg">Total view: ${details.data[0].total_view ? details.data[0].total_view : 'N/A'}</h1>
             <label for="my-modal-5" class="btn">cancel</label>
     </div>
     `;
