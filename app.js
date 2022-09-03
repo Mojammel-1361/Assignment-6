898db8ae27984a81f16d3333bd4cf58fc246a770
mojammel127 <mojammel127@gmail.com>
2022-09-03 10:27:24 +0600

    items call donet

--- app.js
+++ app.js
@@ -21,6 +21,46 @@ loadAllMaun();
 
 // maun part end 
 
-const catagories = (aID) =>{
-    console.log('aID');
+const catagories = async(categoryId) =>{
+    
+const category = await fetch(`https://openapi.programming-hero.com/api/news/category/${categoryId}`);
+    const data = await category.json();
+    const newsItems = data.data;
+    console.log(newsItems);
+    const newsList = document.getElementById('all-data');
+    newsList.innerHTML = '';
+    
+    newsItems.forEach(singleNews => {
+        const {title, img,total_view, author, details, thumbnail_url} = singleNews
+        const newsCard = document.createElement('div');
+        newsCard.classList.add('col')
+        newsCard.innerHTML=`
+            
+      <div class="card lg:card-side bg-base-100 shadow-xl">
+      <figure><img src="${thumbnail_url}" class="h-84 w-64 p-4" alt="Movie"></figure>
+        <div class="card-body">
+            <h2 class="card-title">${title.length > 80 ? title.slice(0,50)+ '...' : title}</h2>
+            <p>${details.length > 900 ? details.slice(0,500)+ '...' : details}</p>
+            
+                
+                <div class="flex flex-row items-center">
+                <div><p><img src="${author.img}" class="h-24 w-24 p-4 rounded-full"</p></div>
+                <div class="basis-1/4 "><p>${author.name}</p></div>
+                <div class="ps-6"><svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path fill="none" stroke="#000" stroke-width="2" d="M12,21 C7,21 1,16 1,12 C1,8 7,3 12,3 C17,3 23,8 23,12 C23,16 17,21 12,21 Z M12,7 C9.23875,7 7,9.23875 7,12 C7,14.76125 9.23875,17 12,17 C14.76125,17 17,14.76125 17,12 C17,9.23875 14.76125,7 12,7 L12,7 Z"/>
+                </svg></div>
+
+                <div class="basis-1/2 p-4">${total_view} M</div>
+                <div class=" p-4"><button class="btn btn-primary">details</button></div>
+                </div>
+                
+                </div>
+                
+            </div>
+        </div>
+        </div>
+        `;
+        newsList.appendChild(newsCard);
+    });
+    
 }
