286e50fc7fe2b40716f40b5e3d53d2713258a715
mojammel127 <mojammel127@gmail.com>
2022-09-03 22:39:35 +0600

    some update

--- app.js
+++ app.js
@@ -41,15 +41,14 @@ const category = await fetch(`https://openapi.programming-hero.com/api/news/cate
       <div class="card lg:card-side bg-base-100 shadow-xl">
       <figure><img src="${thumbnail_url}" class="h-84 w-64 p-4" alt="Movie"></figure>
         <div class="card-body">
-            <h2 class="card-title">${title.length > 80 ? title.slice(0,50)+ '...' : title}</h2>
-            <p>${details.length > 900 ? details.slice(0,500)+ '...' : details}</p>
+            <h2 class="card-title text-purple-500">${title.length > 80 ? title.slice(0,50)+ '....' : title}</h2>
+            <p>${details.length > 900 ? details.slice(0,500)+ '.....' : details}</p>
                 <div class="flex flex-row items-center">
-                <div><p><img src="${author.img}" class="h-24 w-24 p-4 rounded-full"</p></div>
+                <div><p><img src="${author.img}" class="h-20 w-20 p-4 rounded-full"</p></div>
                 <div class="basis-1/4 "><p>${author.name ? author.name : 'N/A'}</p></div>
                 <div class="ps-6"><svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path fill="none" stroke="#000" stroke-width="2" d="M12,21 C7,21 1,16 1,12 C1,8 7,3 12,3 C17,3 23,8 23,12 C23,16 17,21 12,21 Z M12,7 C9.23875,7 7,9.23875 7,12 C7,14.76125 9.23875,17 12,17 C14.76125,17 17,14.76125 17,12 C17,9.23875 14.76125,7 12,7 L12,7 Z"/>
                 </svg></div>
-
                 <div class="basis-1/2 p-4">Total view ${total_view ? total_view : 'N/A'}</div>
                 <label onclick="newsDetail('${_id}')" for="my-modal-5" class="btn modal-button">more...</label>
                 
